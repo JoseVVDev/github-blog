c5c3bbc7729bb5f83872595a8a3a1bce54b70ff0
JoseVVDev <78755947+JoseVVDev@users.noreply.github.com>
2023-04-19 14:42:13 -0300

    ajustes na função HandleDate

--- src/lib/HandleDate.tsx
+++ src/lib/HandleDate.tsx
@@ -1,34 +1,37 @@
 import * as dayjs from "dayjs";
 
 export function HandleDate(date: string) {
-    let timeframe = '';
-
     if (dayjs().diff(date, 'years') > 1){
-        timeframe = `Há ${dayjs().diff(date, 'years')} anos`
-    } else 
-    if (dayjs().diff(date, 'years') > 0){
-        timeframe = `Há 1 ano`
-    } else 
-    if (dayjs().diff(date, 'months') > 1){
-        timeframe = `Há ${dayjs().diff(date, 'months')} meses`
-    } else 
-    if (dayjs().diff(date, 'months') > 0){
-        timeframe = `Há 1 mês`
-    } else 
-    if (dayjs().diff(date, 'weeks') > 1){
-        timeframe = `Há ${dayjs().diff(date, 'weeks')} semanas`
-    } else 
-    if (dayjs().diff(date, 'weeks') > 0){
-        timeframe = `Há 1 semana`
-    } else 
-    if (dayjs().diff(date, 'days') > 1) {
-        timeframe = `Há ${dayjs().diff(date, 'days')} dias`
-    } else 
-    if (dayjs().diff(date, 'days') > 0) {
-        timeframe = `Há 1 dia`
-    } else {
-        timeframe = `Publicado hoje`
+        return (`Há ${dayjs().diff(date, 'years')} anos`)
     }
 
-    return (timeframe)
+    if (dayjs().diff(date, 'years') > 0){
+       return (`Há 1 ano`)
+    } 
+
+    if (dayjs().diff(date, 'months') > 1){
+        return (`Há ${dayjs().diff(date, 'months')} meses`)
+    } 
+
+    if (dayjs().diff(date, 'months') > 0){
+        return (`Há 1 mês`)
+    } 
+
+    if (dayjs().diff(date, 'weeks') > 1){
+        return (`Há ${dayjs().diff(date, 'weeks')} semanas`)
+    } 
+
+    if (dayjs().diff(date, 'weeks') > 0){
+        return (`Há 1 semana`)
+    } 
+
+    if (dayjs().diff(date, 'days') > 1) {
+        return (`Há ${dayjs().diff(date, 'days')} dias`)
+    } 
+    
+    if (dayjs().diff(date, 'days') > 0) {
+        return (`Há 1 dia`)
+    } else {
+        return (`Publicado hoje`)
+    }
 }
